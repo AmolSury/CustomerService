09fe6ef23eaa32bf75f029ed067b3eaa5aca27c6
Amol Tukaram Suryavanshi <AS00587213@INPUSCPC10460.TechMahindra.com>
2018-12-27 17:30:38 +0530

    scheduling

--- src/main/java/com/main/controller/CustomerServiceApplication.java
+++ src/main/java/com/main/controller/CustomerServiceApplication.java
@@ -3,12 +3,16 @@ package com.main.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableScheduling
+@EnableCaching
+@EnableEurekaClient
 @ComponentScan({"com.main.controller", "com.main.services"})
 @EnableJpaRepositories("com.main.repository")
 @EntityScan("com.main.entity")
